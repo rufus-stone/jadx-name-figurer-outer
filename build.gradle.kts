ecb906ac40dfb5d8683ecac216c4462dd3acc006
Rufus Stone <60827786+rufus-stone@users.noreply.github.com>
2025-02-21 09:53:45 -0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 }
 
 dependencies {
-	val jadxVersion = "1.5.1-SNAPSHOT"
+	val jadxVersion = "1.5.2-SNAPSHOT"
 	val isJadxSnapshot = jadxVersion.endsWith("-SNAPSHOT")
 
 	// use compile only scope to exclude jadx-core and its dependencies from result jar
